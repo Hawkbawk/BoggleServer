64928c4cfbe6ff09cafe471da89aeb6b02b823bd
Ryan Hawkins <u1105702@umail.utah.edu>
2019-04-01 13:55:08 -0600

    Reorganized and formatted code using CodeMaid

--- BoggleService/BoggleService/Models/Game.cs
+++ BoggleService/BoggleService/Models/Game.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace BoggleService.Models
+﻿namespace BoggleService.Models
 {
     public class Game
     {
